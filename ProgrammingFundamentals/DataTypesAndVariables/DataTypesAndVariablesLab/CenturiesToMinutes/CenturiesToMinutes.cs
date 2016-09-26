63f83bc216e0476ace036688f75f82d97285f9ec
tanya-ivanova <ivanova.itanya@gmail.com>
2016-09-26 21:32:13 +0300

    Changing back CenturiesToMinutes.cs

--- ProgrammingFundamentals/DataTypesAndVariables/DataTypesAndVariablesLab/CenturiesToMinutes/CenturiesToMinutes.cs
+++ ProgrammingFundamentals/DataTypesAndVariables/DataTypesAndVariablesLab/CenturiesToMinutes/CenturiesToMinutes.cs
@@ -10,7 +10,7 @@ namespace CenturiesToMinutes
     {
         static void Main(string[] args)
         {
-            byte centuries = byte.Parse(Console.ReadLine()); // adding comment
+            byte centuries = byte.Parse(Console.ReadLine());
             short years = (short)(centuries * 100);
             int days = (int)(years * 365.2422);
             long hours = (long)(days * 24);
